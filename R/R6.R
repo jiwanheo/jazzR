d483c16e9d9c6038f1c91f95abaa004ee73470d8
Jiwan Heo <jiwanheo123@gmail.com>
2022-02-21 22:50:38 -0500

    document: R6

--- R/R6.R
+++ R/R6.R
@@ -1,16 +1,17 @@
-#' R6
+#' R6 class of drug process
 #'
-#' R6 class to help with data processing
+#' R6 object to encapsulate the parameters passing around modules.
 #'
-#' @return The return value, if any, from executing the function.
-#'
-#' @noRd
 #' @importFrom R6 R6Class
 
 JazzR6 <- R6Class(
   "JazzR6",
   public = list(
+    #' @field drug_name Name of the drug to be analyzed. This is subject to
+    #' change, every time a new drug is entered.
     drug_name = NULL,
+    #' @field trial_name Name of the trial to be analyzed. This is subject to
+    #' change, every time a new trial is entered.
     trial_name = NULL
   )
 )
